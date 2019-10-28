48d8b82fe2322b3afa27cf08a196172fb4d76ee8
itswcg <itswcg@gmail.com>
2019-10-28 20:29:40 +0800

    feat(middleware): add token middleware

--- middleware/auth/auth.go
+++ middleware/auth/auth.go
@@ -1,19 +1,49 @@
 package auth
 
+import (
+	bm "github.com/bilibili/kratos/pkg/net/http/blademaster"
+	"github.com/bilibili/kratos/pkg/net/rpc/warden"
+	accrpc "github.com/itswcg/micro/account/api"
+)
+
 type Config struct {
-	IsAccount bool
+	IsAccount     bool
+	AccountClient *warden.ClientConfig
 }
 
 type Auth struct {
 	config *Config
+	ac     accrpc.AccountClient
 }
 
 func New(conf *Config) *Auth {
 	if conf == nil {
-		conf = &Config{IsAccount: false}
+		conf = &Config{IsAccount: false, AccountClient: nil}
 	}
-	auth := &Auth{
-		conf,
+	auth := &Auth{config: conf}
+	if auth.config.IsAccount == false {
+		ac, err := accrpc.NewClient(auth.config.AccountClient)
+		if err != nil {
+			panic(err)
+		}
+		auth.ac = ac
 	}
+
 	return auth
+}
+
+func (a *Auth) authToken(ctx *bm.Context) (mid int64, err error) {
+	return
+}
+
+func (a *Auth) IsAdmin(ctx *bm.Context) (mid int64, err error) {
+	return
+}
+
+func (a *Auth) IsAuthenticated(ctx *bm.Context) (mid int64, err error) {
+	return
+}
+
+func (a *Auth) AllowAny(ctx *bm.Context) (mid int64, err error) {
+	return
 }
--- middleware/go.mod
+++ middleware/go.mod
@@ -1,4 +1,4 @@
-module middleware
+module github.com/itswcg/micro/middleware
 
 go 1.12
 
@@ -6,6 +6,9 @@ require (
 	github.com/bilibili/kratos v0.2.4-0.20191025092737-e14170de04ba
 	github.com/gogo/protobuf v1.3.0
 	github.com/golang/protobuf v1.3.2
+	github.com/itswcg/micro/account v0.0.0-00010101000000-000000000000 // indirect
 	golang.org/x/net v0.0.0-20191011234655-491137f69257
 	google.golang.org/grpc v1.24.0
 )
+
+replace github.com/itswcg/micro/account => ../account

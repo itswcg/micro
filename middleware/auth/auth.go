71e154ba2764ae2f8c8b871c6449865884fa7fd6
itswcg <itswcg@gmail.com>
2019-10-29 11:07:30 +0800

    feat(middleware): set token to Authorization

--- middleware/auth/auth.go
+++ middleware/auth/auth.go
@@ -1,49 +1,71 @@
 package auth
 
 import (
+	"github.com/bilibili/kratos/pkg/ecode"
 	bm "github.com/bilibili/kratos/pkg/net/http/blademaster"
+	"github.com/bilibili/kratos/pkg/net/metadata"
 	"github.com/bilibili/kratos/pkg/net/rpc/warden"
-	accrpc "github.com/itswcg/micro/account/api"
+	authrpc "github.com/itswcg/micro/auth/api"
 )
 
 type Config struct {
-	IsAccount     bool
-	AccountClient *warden.ClientConfig
+	AuthClient *warden.ClientConfig
 }
 
 type Auth struct {
 	config *Config
-	ac     accrpc.AccountClient
+	ac     authrpc.AuthClient
 }
 
 func New(conf *Config) *Auth {
-	if conf == nil {
-		conf = &Config{IsAccount: false, AccountClient: nil}
-	}
 	auth := &Auth{config: conf}
-	if auth.config.IsAccount == false {
-		ac, err := accrpc.NewClient(auth.config.AccountClient)
-		if err != nil {
-			panic(err)
-		}
-		auth.ac = ac
-	}
 
+	ac, err := authrpc.NewClient(auth.config.AuthClient)
+	if err != nil {
+		panic(err)
+	}
+	auth.ac = ac
 	return auth
 }
 
-func (a *Auth) authToken(ctx *bm.Context) (mid int64, err error) {
-	return
+func (a *Auth) authToken(ctx *bm.Context, token string) (mid int64, err error) {
+	tokenReq := &authrpc.TokenReq{Token: token}
+	tokenReply, err := a.ac.Token(ctx, tokenReq)
+	if err != nil {
+		return 0, err
+	}
+	return tokenReply.Mid, nil
 }
 
-func (a *Auth) IsAdmin(ctx *bm.Context) (mid int64, err error) {
-	return
+func (a *Auth) IsAdmin(ctx *bm.Context) {
+
 }
 
-func (a *Auth) IsAuthenticated(ctx *bm.Context) (mid int64, err error) {
-	return
+func (a *Auth) IsAuthenticated(ctx *bm.Context) {
+	req := ctx.Request
+	token := req.Header.Get("Authorization")
+	if token == "" {
+		ctx.JSON(nil, ecode.Unauthorized)
+		ctx.Abort()
+		return
+	}
+	mid, err := a.authToken(ctx, token)
+	if err != nil {
+		ctx.JSON(nil, ecode.Unauthorized)
+		ctx.Abort()
+		return
+	}
+	a.setMid(ctx, mid)
 }
 
-func (a *Auth) AllowAny(ctx *bm.Context) (mid int64, err error) {
-	return
+func (a *Auth) AllowAny(ctx *bm.Context) {
+
+}
+
+func (a *Auth) setMid(ctx *bm.Context, mid int64) {
+	ctx.Set(metadata.Mid, mid)
+	if md, ok := metadata.FromContext(ctx); ok {
+		md[metadata.Mid] = mid
+		return
+	}
 }
--- middleware/go.mod
+++ middleware/go.mod
@@ -6,9 +6,9 @@ require (
 	github.com/bilibili/kratos v0.2.4-0.20191025092737-e14170de04ba
 	github.com/gogo/protobuf v1.3.0
 	github.com/golang/protobuf v1.3.2
-	github.com/itswcg/micro/account v0.0.0-00010101000000-000000000000 // indirect
+	github.com/itswcg/micro/auth v0.0.0-00010101000000-000000000000 // indirect
 	golang.org/x/net v0.0.0-20191011234655-491137f69257
 	google.golang.org/grpc v1.24.0
 )
 
-replace github.com/itswcg/micro/account => ../account
+replace github.com/itswcg/micro/auth => ../auth
